c556371b9dbfc5bf4566e2da7c7dfeded01d0957
Erdem Toruk <erdemtoruk26@gmail.com>
2025-08-25 14:57:10 +0300

    Navigation Menu Created
    Issue #7

---
+++ AcademicPlanner/AcademicPlanner/MainDashboard.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace AcademicPlanner
+{
+    partial class MainDashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tableLayoutPanel1 = new TableLayoutPanel();
+            ıconButton6 = new FontAwesome.Sharp.IconButton();
+            ıconButton5 = new FontAwesome.Sharp.IconButton();
+            ıconButton4 = new FontAwesome.Sharp.IconButton();
+            ıconButton3 = new FontAwesome.Sharp.IconButton();
+            ıconButton2 = new FontAwesome.Sharp.IconButton();
+            MainMenu_btn = new FontAwesome.Sharp.IconButton();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 6;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 16.666666F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 16.666666F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 16.666666F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 16.666666F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 16.666666F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 16.666666F));
+            tableLayoutPanel1.Controls.Add(ıconButton6, 5, 0);
+            tableLayoutPanel1.Controls.Add(ıconButton5, 4, 0);
+            tableLayoutPanel1.Controls.Add(ıconButton4, 3, 0);
+            tableLayoutPanel1.Controls.Add(ıconButton3, 2, 0);
+            tableLayoutPanel1.Controls.Add(ıconButton2, 1, 0);
+            tableLayoutPanel1.Controls.Add(MainMenu_btn, 0, 0);
+            tableLayoutPanel1.Dock = DockStyle.Top;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 1;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.Size = new Size(1114, 56);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // ıconButton6
+            // 
+            ıconButton6.Dock = DockStyle.Fill;
+            ıconButton6.IconChar = FontAwesome.Sharp.IconChar.Bars;
+            ıconButton6.IconColor = Color.Black;
+            ıconButton6.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            ıconButton6.ImageAlign = ContentAlignment.MiddleLeft;
+            ıconButton6.Location = new Point(928, 3);
+            ıconButton6.Name = "ıconButton6";
+            ıconButton6.Size = new Size(183, 50);
+            ıconButton6.TabIndex = 5;
+            ıconButton6.Text = "Settings";
+            ıconButton6.UseVisualStyleBackColor = true;
+            // 
+            // ıconButton5
+            // 
+            ıconButton5.Dock = DockStyle.Fill;
+            ıconButton5.IconChar = FontAwesome.Sharp.IconChar.None;
+            ıconButton5.IconColor = Color.Black;
+            ıconButton5.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            ıconButton5.Location = new Point(743, 3);
+            ıconButton5.Name = "ıconButton5";
+            ıconButton5.Size = new Size(179, 50);
+            ıconButton5.TabIndex = 4;
+            ıconButton5.Text = ".. Page";
+            ıconButton5.UseVisualStyleBackColor = true;
+            // 
+            // ıconButton4
+            // 
+            ıconButton4.Dock = DockStyle.Fill;
+            ıconButton4.IconChar = FontAwesome.Sharp.IconChar.ChartLine;
+            ıconButton4.IconColor = Color.Black;
+            ıconButton4.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            ıconButton4.ImageAlign = ContentAlignment.MiddleLeft;
+            ıconButton4.Location = new Point(558, 3);
+            ıconButton4.Name = "ıconButton4";
+            ıconButton4.Size = new Size(179, 50);
+            ıconButton4.TabIndex = 3;
+            ıconButton4.Text = ".. Page";
+            ıconButton4.UseVisualStyleBackColor = true;
+            // 
+            // ıconButton3
+            // 
+            ıconButton3.Dock = DockStyle.Fill;
+            ıconButton3.IconChar = FontAwesome.Sharp.IconChar.None;
+            ıconButton3.IconColor = Color.Black;
+            ıconButton3.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            ıconButton3.Location = new Point(373, 3);
+            ıconButton3.Name = "ıconButton3";
+            ıconButton3.Size = new Size(179, 50);
+            ıconButton3.TabIndex = 2;
+            ıconButton3.Text = ".. Page";
+            ıconButton3.UseVisualStyleBackColor = true;
+            // 
+            // ıconButton2
+            // 
+            ıconButton2.Dock = DockStyle.Fill;
+            ıconButton2.IconChar = FontAwesome.Sharp.IconChar.None;
+            ıconButton2.IconColor = Color.Black;
+            ıconButton2.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            ıconButton2.Location = new Point(188, 3);
+            ıconButton2.Name = "ıconButton2";
+            ıconButton2.Size = new Size(179, 50);
+            ıconButton2.TabIndex = 1;
+            ıconButton2.Text = ".. Page";
+            ıconButton2.UseVisualStyleBackColor = true;
+            // 
+            // MainMenu_btn
+            // 
+            MainMenu_btn.Dock = DockStyle.Fill;
+            MainMenu_btn.IconChar = FontAwesome.Sharp.IconChar.HomeUser;
+            MainMenu_btn.IconColor = Color.Black;
+            MainMenu_btn.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            MainMenu_btn.ImageAlign = ContentAlignment.MiddleLeft;
+            MainMenu_btn.Location = new Point(3, 3);
+            MainMenu_btn.Name = "MainMenu_btn";
+            MainMenu_btn.Size = new Size(179, 50);
+            MainMenu_btn.TabIndex = 0;
+            MainMenu_btn.Text = "Main Menu";
+            MainMenu_btn.UseVisualStyleBackColor = true;
+            MainMenu_btn.Click += MainMenu_btn_Click;
+            // 
+            // MainDashboard
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(2, 52, 64);
+            ClientSize = new Size(1114, 567);
+            Controls.Add(tableLayoutPanel1);
+            Name = "MainDashboard";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Main Dashboard";
+            Load += MainDashboard_Load;
+            Resize += MainDashboard_Resize;
+            tableLayoutPanel1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private FontAwesome.Sharp.IconButton ıconButton6;
+        private FontAwesome.Sharp.IconButton ıconButton5;
+        private FontAwesome.Sharp.IconButton ıconButton4;
+        private FontAwesome.Sharp.IconButton ıconButton3;
+        private FontAwesome.Sharp.IconButton ıconButton2;
+        private FontAwesome.Sharp.IconButton MainMenu_btn;
+    }
+}
---
+++ AcademicPlanner/AcademicPlanner/MainDashboard.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AcademicPlanner
+{
+    public partial class MainDashboard : Form
+    {
+        public MainDashboard()
+        {
+            InitializeComponent();
+        }
+
+
+        /****************************************************************** Responsive *****************************************************************************/
+        private void Size_Change()
+        {
+            int width = this.ClientSize.Width > 0 ? this.ClientSize.Width : 1000;
+            int height = this.ClientSize.Height > 0 ? this.ClientSize.Height : 700;
+
+            tableLayoutPanel1.Height = Convert.ToInt32(height / 10);
+        }
+
+        private void MainDashboard_Load(object sender, EventArgs e)
+        {
+            Size_Change();
+        }
+
+        private void MainDashboard_Resize(object sender, EventArgs e)
+        {
+            Size_Change();
+        }
+
+        /***************************************************************** Form Change ******************************************************************************/
+
+        private void MainMenu_btn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
